fa1005977629289e148b70c619abaf8b03b865c3
ConanGH-S <blandon0207s@gmail.com>
2022-10-11 22:40:45 -0500

    sumador de parametros con Typescript

---
+++ main.ts
@@ -0,0 +1,5 @@
+const sumador = (n1: number, n2: number) => {
+	return console.log(`La suma del numero ${n1} con el numero ${n2} es de ${n1 + n2}`);
+};
+console.log('Bienvenido a un sumador de dos números');
+sumador(5, 6);
